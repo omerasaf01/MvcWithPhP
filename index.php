13d15d1f296d11954a145f80c7542d4e98d0af5f
Ömer Asaf Karasu <omerconsept999@gmail.com>
2024-03-31 22:33:42 +0300

    Tailwind integration added

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ require "vendor/autoload.php";
 
 if (file_exists("Controllers/" . $_SERVER["REQUEST_URI"] . "Controller.php")) {
     require "Controllers/" . $_SERVER["REQUEST_URI"] . "Controller.php";
+} else {
+    require "Controllers/DefaultController.php";
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.twig'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
